b9f4434d78d6a2e0e9d678f025a354dc0194682d
Kettengeschwindigkeit <666damned999@mail.ru>
2022-07-12 14:54:39 +0300

    inserted expressions

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,8 +1,20 @@
 
 export const Page = () => {
+    
+    const logoUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmg6VbQr7k4bE8m1sGjODK19nEZn-UKVChBg&usqp=CAU"
+    const sitename = "example.com"
+
+    const getHeadLine = () => {
+        return logoUrl ? (
+            <img src={logoUrl} alt="logo" />
+        ) : (
+            <div>{sitename}</div>
+        )
+    }
+
     return (
         <>
-            <header>Header</header>
+            <header>{getHeadLine()}</header>
             <footer>Footer</footer>
         </>
     )
